fd2e0ae95e55adee110be99dd95cba6536f674e3
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2026-03-12 00:39:46 -0600

    refactor: update path aliases in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@public': '/public',
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@public': path.resolve(__dirname, 'public'),
     },
   },
 })
